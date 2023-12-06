36eb7e4eed5b647f23b6bc3c49a68b83d01c08fa
Tanmay Bansal <143084365+Tanmay170@users.noreply.github.com>
2023-12-06 14:27:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
